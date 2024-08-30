5d711d8ae5fc909f16fadba3341b857800b57f05
geko <gekko@geko.boo>
2024-08-30 19:41:29 +0200

    Update rust config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,5 +2,10 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     format = { timeout_ms = 5000 },
+    setup = {
+      rust_analyzer = function()
+        return true
+      end,
+    },
   },
 }
